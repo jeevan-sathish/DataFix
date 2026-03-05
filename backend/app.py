ceefe5c8ad6d34e24bd533fd6f1bbf012aebd725
Jeevan s <sathishjeevan2004@gmail.com>
2026-03-05 20:18:31 +0530

    learnined how to load dataset ,how to call data and save files by filtering creating files etc

--- backend/app.py
+++ backend/app.py
@@ -1,4 +1,7 @@
 import pandas as pd
 
 df = pd.read_csv("datasets/Titanic-Dataset.csv")
-print(df.head())
+survived =df[df["Survived"]==1]
+unsurvived =df[df["Survived"]==0]
+survived.to_csv("saved_dataset/survived_dataset.csv",index=False)
+unsurvived.to_csv("saved_dataset/unsurvived_dataset.csv" , index=False)
