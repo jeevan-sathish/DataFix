f9deb582b2cb1d005e7cf5a1660ad629fee44e7e
Jeevan s <sathishjeevan2004@gmail.com>
2026-03-06 13:48:09 +0530

    sample data displaying from client to server and server to client

--- backend/app.py
+++ backend/app.py
@@ -1,7 +1,7 @@
-from flask import Flask ,request,jsonify
-from flask_cors import CORS 
-import pandas as pd 
 import numpy as np
+import pandas as pd
+from flask import Flask,request,jsonify
+from flask_cors import CORS
 
 app =Flask(__name__)
 CORS(app)
@@ -12,17 +12,17 @@ def display_data():
     if(file):
         df =pd.read_csv(file)
         print(df.head())
-        df.replace({np.nan:None})
-        result= df.head().to_dict(orient="records")
+        df =df.replace({np.nan:None})
+        result =df.head().to_dict(orient="records")
         return jsonify({
             "data":result
         })
-        
     else:
         return jsonify({
-        "message":"no file deen uploaded"
+            "message":"no file was uploaded"
         })
 
 
-if __name__ =="__main__":
+
+if __name__=="__main__":
     app.run(debug=True)
