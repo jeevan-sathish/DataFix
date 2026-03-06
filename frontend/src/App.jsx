07aefe15d2c44ccecfeaa37b894cd174f9a50151
Jeevan s <sathishjeevan2004@gmail.com>
2026-03-06 13:55:55 +0530

    .

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,29 +2,29 @@ import React, { useState } from 'react'
 
 const App = () => {
   const [file,setFile]=useState(null)
-  const [data,setData]=useState();
+  const [data,setData]=useState(null)
 
-  const handleFileUpload=(e)=>{
+  const handleFileUpload =(e)=>{
     setFile(e.target.files[0])
-
   }
 
-  const handleUpload =async ()=>{
-    const formData =new FormData()
+  const handleFileSubmit =async ()=>{
+    const formData = new FormData()
     formData.append("file",file)
 
-    const res = await fetch("http://127.0.0.1:5000/upload",{
+    const res =await fetch("http://127.0.0.1:5000/upload",{
       method:"POST",
       body:formData
     })
-    const result =await res.json()
-    setData(result.data)
+
+    const result = await res.json()
+    setData(result)
 
   }
   return (
     <div>
       <input type="file" accept='.csv' onChange={handleFileUpload} />
-      <button onClick={handleUpload}>upload</button>
+      <button onClick={handleFileSubmit}>upload</button>
       <pre>{JSON.stringify(data,null,2)}</pre>
     </div>
   )
