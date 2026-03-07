e53d47ace1f9d69e50064d12e97bc876e71cda52
Jeevan s <sathishjeevan2004@gmail.com>
2026-03-07 20:23:14 +0530

    just blank server run

--- backend/load_balance.py
+++ backend/load_balance.py
@@ -1,24 +1,17 @@
-from flask import Flask,request
+from flask import Flask
 import requests
 
-app=Flask(__name__)
+app = Flask(__name__)
 
-server =[
-    "http://localhost:5001",
-     "http://localhost:5002",
-      "http://localhost:5003",
-]
-index =0
+
+
 @app.route('/')
 def load_balancer():
-    global index
-    
-    
-    server =server[index]
-    responce = requests.get(server)
-    index =(index+1) % len(server)
-    
-    return responce.text
+  
 
-if __name__=="__main__":
-    app.run(port=8000)
+
+    return "hello world"
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
