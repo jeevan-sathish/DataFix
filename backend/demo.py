f1c9a784309da3d95adcdba13850775220670fc5
Jeevan s <sathishjeevan2004@gmail.com>
2026-03-07 20:06:40 +0530

    learning load balancing

--- backend/demo.py
+++ backend/demo.py
@@ -7,21 +7,31 @@
 # # inp =str(input("enter the items :"))
 
 
-names = ["jeevan","ravan","raju","somesh","chintu"]
-data ={
+# names = ["jeevan","ravan","raju","somesh","chintu"]
+# data ={
     
-}
-inp =str(input("enter the name"))
-for keys, values in data.items():
-    if str(keys).lower() == inp:
-        print(keys,"found")
-    else:
-        data[inp]=inp
+# }
+# inp =str(input("enter the name"))
+# for keys, values in data.items():
+#     if str(keys).lower() == inp:
+#         print(keys,"found")
+#     else:
+#         data[inp]=inp
         
 
 
-print(data.items())
+# print(data.items())
             
+servers = ["Server1", "Server2", "Server3"]
+
+requests = ["Req1", "Req2", "Req3", "Req4", "Req5", "Req6"]
+
+index = 0
+
+for req in requests:
+    server = servers[index]
+    print(req, "handled by", server)
+    index = (index + 1) % len(servers)
             
         
     
